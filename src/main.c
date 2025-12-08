1b657ceb13d042683dcc5b2dfe470f4b427aec9f
Tyler LePage <lepagetyler@hotmail.com>
2025-12-07 23:45:35 -0600

    Added a clear color to the viewport

--- src/main.c
+++ src/main.c
@@ -1,16 +1,34 @@
-#include <GLFW/glfw3.h>
 #include <stdio.h>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main() {
-    if (glfwInit() != GLFW_TRUE) {
-        printf("Failed to initialize glfw!");
+    if (!glfwInit()) {
+        printf("Failed to initialize glfw!\n");
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow *window = glfwCreateWindow(800, 600, "everest", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("Failed to load GL with glad!\n");
+        return -1;
+    }
     
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        glViewport(0, 0, 800, 600);
+
+        glClearColor(0.35f, 0.15f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();

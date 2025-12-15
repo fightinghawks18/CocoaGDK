aa900d815a7b68899a3081c7d25e1556e9aaf1ed
Tyler LePage <lepagetyler@hotmail.com>
2025-12-14 20:26:27 -0600

    Added opengl library to speed up graphics development.

---
+++ src/opengl/include/opengl/opengl_context.h
@@ -0,0 +1,15 @@
+//
+// Created by fightinghawks18 on 12/14/25.
+//
+
+#pragma once
+
+#include "core/core_types.h"
+
+typedef struct CcoOpenGLContext_T CcoOpenGLContext_T;
+typedef CcoOpenGLContext_T *CcoOpenGLContext;
+
+CcoResult ccoCreateOpenGLContext(void *windowHandle, void *displayHandle, CcoOpenGLContext *outOpenGLContext);
+void ccoDestroyOpenGLContext(CcoOpenGLContext openGLContext);
+void ccoMakeCurrentOpenGLContext(CcoOpenGLContext openGLContext);
+void ccoRemoveCurrentOpenGLContext();
---
+++ src/opengl/src/opengl_context_macos.m
@@ -0,0 +1,39 @@
+//
+// Created by fightinghawks18 on 12/14/25.
+//
+
+#include "opengl/opengl_context.h"
+#include <AppKit/AppKit.h>
+
+struct CcoOpenGLContext_T {
+    NSOpenGLContext *ctx;
+};
+
+CcoResult ccoCreateOpenGLContext(void *windowHandle, void *displayHandle, CcoOpenGLContext *outOpenGLContext) {
+    CcoOpenGLContext openGLContext = malloc(sizeof(CcoOpenGLContext_T));
+    if (!openGLContext)
+        return CCO_FAIL_OUT_OF_MEMORY;
+    NSOpenGLPixelFormatAttribute attrs[] = {NSOpenGLPFADoubleBuffer,  NSOpenGLPFADepthSize,          24,
+                                            NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion4_1Core, 0};
+
+    NSOpenGLPixelFormat *pxFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+    NSOpenGLContext *glContext = [[NSOpenGLContext alloc] initWithFormat:pxFormat shareContext:nil];
+    [pxFormat release];
+
+    [glContext setView:[(__bridge NSWindow *)windowHandle contentView]];
+    openGLContext->ctx = glContext;
+
+    *outOpenGLContext = openGLContext;
+    return CCO_SUCCESS;
+}
+
+void ccoDestroyOpenGLContext(CcoOpenGLContext openGLContext) {
+    if (openGLContext->ctx != nil) {
+        [openGLContext->ctx release];
+        openGLContext->ctx = nil;
+    }
+    free(openGLContext);
+}
+
+void ccoMakeCurrentOpenGLContext(CcoOpenGLContext openGLContext) { [openGLContext->ctx makeCurrentContext]; }
+void ccoRemoveCurrentOpenGLContext() { [NSOpenGLContext clearCurrentContext]; }

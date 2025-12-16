bf6e83755ae9c2496047ad26b70a236bf3aa339d
fightinghawks18 <lepagetyler@hotmail.com>
2025-12-15 18:58:30 -0600

    Added windowing for win32 platform

---
+++ src/platform/src/win32/windowing_win32.c
@@ -0,0 +1,80 @@
+//
+// Created by fightinghawks18 on 12/15/2025.
+//
+
+#include "platform/windowing.h"
+
+#include <windows.h>
+
+typedef struct CcoWindow_T {
+    HWND hWnd;
+    bool shouldClose;
+} CcoWindow_T;
+
+LRESULT Wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    CcoWindow window = CCO_NULL_HANDLE;
+    if (uMsg == WM_NCCREATE) {
+        CREATESTRUCT *cs = (CREATESTRUCT *)lParam;
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)cs->lpCreateParams);
+    }
+    window = (CcoWindow)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+
+    if (uMsg == WM_DESTROY) {
+        window->shouldClose = true;
+        return FALSE;
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+CcoResult ccoWindowingInit() {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = &Wndproc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = "CocoaWindow";
+    if (!RegisterClass(&wc)) {
+        CCO_LOG("Windows failed to create a new window class!");
+        return CCO_FAIL_WINDOWING_INIT_ERROR;
+    }
+
+    return CCO_SUCCESS;
+}
+
+void ccoWindowingPoll() {
+    MSG msg;
+    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+void ccoWindowingQuit() { PostQuitMessage(0); }
+
+CcoResult ccoCreateWindow(const CcoWindowDesc *desc, CcoWindow *outWindow) {
+    CcoWindow window = malloc(sizeof(CcoWindow_T));
+    if (!window)
+        return CCO_FAIL_OUT_OF_MEMORY;
+    HWND hWnd = CreateWindowEx(0, "CocoaWindow", desc->title, WS_OVERLAPPEDWINDOW, desc->x, desc->y, desc->w, desc->h,
+                               NULL, NULL, GetModuleHandle(NULL), &window);
+    if (!hWnd) {
+        CCO_LOG("Windows failed to create window!");
+        return CCO_FAIL_WINDOWING_CREATE_ERROR;
+    }
+
+    window->hWnd = hWnd;
+    window->shouldClose = false;
+
+    *outWindow = window;
+    return CCO_SUCCESS;
+}
+
+void ccoSetWindowShouldClose(CcoWindow window, bool close) { window->shouldClose = close; }
+
+void ccoCloseWindow(CcoWindow window) {
+    if (IsWindow(window->hWnd)) {
+        DestroyWindow(window->hWnd);
+    }
+    free(window);
+}
+
+CcoWindowNativeHandle ccoGetNativeWindowHandle(CcoWindow window) { return window->hWnd; }
+
+bool ccoShouldWindowClose(CcoWindow window) { return window->shouldClose; }

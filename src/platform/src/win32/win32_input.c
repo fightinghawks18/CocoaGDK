6480a0cd5a04e7c1968add3031ad64f6b9b5265a
fightinghawks18 <lepagetyler@hotmail.com>
2025-12-20 19:53:27 -0600

    Added void to empty method signatures to avoid allowing unspecified amounts of arguments, added documentation to window.h methods, added missing win32 platform files, and added Wndproc in win32_window.c

---
+++ src/platform/src/win32/win32_input.c
@@ -0,0 +1,6 @@
+//
+// Created by fightinghawks18 on 12/20/2025.
+//
+
+#include "platform/win32/win32_input.h"
+#include "platform/input.h"
--- src/platform/src/win32/win32_window.c
+++ src/platform/src/win32/win32_window.c
@@ -1,3 +1,40 @@
 //
 // Created by fightinghawks18 on 12/19/25.
 //
+
+#include "platform/window.h"
+#include <windows.h>
+#include "platform/win32/win32_input.h"
+
+struct CcoWindow_T {
+    HWND hWnd;
+    CcoBool willClose;
+    CcoBool focused;
+};
+
+LRESULT CALLBACK Wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    CcoWindow window = CCO_NIL;
+    if (uMsg == WM_NCCREATE) {
+        CREATESTRUCT *createStruct = (CREATESTRUCT *)lParam;
+        window = (CcoWindow)createStruct->lpCreateParams;
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)window);
+    } else {
+        window = (CcoWindow)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+    }
+
+    switch (uMsg) {
+    case WM_INPUT: {
+        ccoInputHandleRawInput(hWnd, lParam);
+        return 0;
+    }
+    case WM_CLOSE: {
+        window->willClose = CCO_YES;
+        return 0;
+    }
+    default: break;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+

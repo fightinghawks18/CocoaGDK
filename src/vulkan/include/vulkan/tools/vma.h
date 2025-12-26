c945f92974fcd83958f68f34276a31af87616865
Tyler LePage <lepagetyler@hotmail.com>
2025-12-26 13:23:01 -0600

    Fixed VMA include not working on other platforms, and hid all VMA warnings due to some compilers sending thousands of warnings.

--- src/vulkan/include/vulkan/tools/vma.h
+++ src/vulkan/include/vulkan/tools/vma.h
@@ -4,4 +4,29 @@
 
 #pragma once
 
+#if defined(__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wall"
+    #pragma clang diagnostic ignored "-Wextra"
+    #pragma clang diagnostic ignored "-Wnullability-completeness"
+#elif defined(__GNUC__) || defined(__GNUG__)
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wall"
+    #pragma GCC diagnostic ignored "-Wextra"
+#elif defined(_MSC_VER)
+    #pragma warning(push, 0)
+#endif
+
+#ifdef _WIN32
 #include <vma/vk_mem_alloc.h>
+#else
+#include <vk_mem_alloc.h>
+#endif
+
+#if defined(__clang__)
+    #pragma clang diagnostic pop
+#elif defined(__GNUC__) || defined(__GNUG__)
+    #pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+    #pragma warning(pop)
+#endif
